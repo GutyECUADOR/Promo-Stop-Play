d30df658ea9fe1a21bfa37d57cadbfe52eac2910
José L. Gutiérrez <gutiecuador@gmail.com>
2021-03-10 18:57:01 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# promo-stop-play
+ Proyecto Informativo de Promo con Tetrapack
---
+++ views/modulos/bienvenidosView.php
@@ -0,0 +1,89 @@
+<div class="main-container">
+  <section class="space-sm">
+    <div class="container">
+      <div class="row">
+        <div class="col text-center">
+          <img alt="Image" src="assets/img/logo-gray.svg" />
+        </div>
+        <!--end of col-->
+      </div>
+      <!--end of row-->
+    </div>
+    <!--end of container-->
+  </section>
+  <section class="height-100 flush-with-above">
+    <div class="container">
+      <div class="row justify-content-between align-items-center">
+        <div class="col-12 text-center">
+          <img alt="Image" src="assets/img/graphic-woman-writing.svg" class="w-50" />
+        </div>
+        <!--end of col-->
+        <div class="col-12 col-md-7 col-lg-4 mb-4 mt-4 text-center text-md-left">
+          <img alt="Image" src="assets/img/Bandera-colombia.png" class="w-50" />
+          
+        </div>
+        <!--end of col-->
+      </div>
+      <!--end of row-->
+    </div>
+    <!--end of container-->
+  </section>
+  <!--end of section-->
+  <section class="space-sm flush-with-above">
+    <div class="container">
+      <div class="row">
+        <div class="col text-center">
+          <span class="text-muted text-small">Derechos Reservados ☕ <?php echo date('Y')?></span>
+        </div>
+        <!--end of col-->
+      </div>
+      <!--end of row-->
+    </div>
+    <!--end of container-->
+  </section>
+</div>
+
+<div class="modal fade" id="video-modal" tabindex="-1" aria-hidden="true">
+  <div class="modal-dialog modal-lg modal-center-viewport">
+    <div class="modal-content">
+      <div class="embed-responsive embed-responsive-16by9">
+        <iframe class="embed-responsive-item" data-src=_global.iframeSrc allowfullscreen></iframe>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Required vendor scripts (Do not remove) -->
+<script type="text/javascript" src="assets/js/jquery.min.js"></script>
+<script type="text/javascript" src="assets/js/popper.min.js"></script>
+<script type="text/javascript" src="assets/js/bootstrap.js"></script>
+
+<!-- Optional Vendor Scripts (Remove the plugin script here and comment initializer script out of index.js if site does not use that feature) -->
+
+<!-- AOS (Animate On Scroll - animates elements into view while scrolling down) -->
+<script type="text/javascript" src="assets/js/aos.js"></script>
+<!-- Flatpickr (calendar/date/time picker UI) -->
+<script type="text/javascript" src="assets/js/flatpickr.min.js"></script>
+<!-- Flickity (handles touch enabled carousels and sliders) -->
+<script type="text/javascript" src="assets/js/flickity.pkgd.min.js"></script>
+<!-- jarallax (parallax effect and video backgrounds) -->
+<script type="text/javascript" src="assets/js/jarallax.min.js"></script>
+<script type="text/javascript" src="assets/js/jarallax-video.min.js"></script>
+<script type="text/javascript" src="assets/js/jarallax-element.min.js"></script>
+<!-- ScrollMonitor (manages events for elements scrolling in and out of view) -->
+<script type="text/javascript" src="assets/js/scrollMonitor.js"></script>
+<!-- jQuery smartWizard facilitates steppable wizard content -->
+<script type="text/javascript" src="assets/js/jquery.smartWizard.min.js"></script>
+<!-- Smooth scroll (animation to links in-page)-->
+<script type="text/javascript" src="assets/js/smooth-scroll.polyfills.min.js"></script>
+<!-- Prism (displays formatted code boxes) -->
+<script type="text/javascript" src="assets/js/prism.js"></script>
+<script type="text/javascript" src="assets/js/zoom.min.js"></script>
+
+<!-- Required theme scripts (Do not remove) -->
+<script type="text/javascript" src="assets/js/theme.js"></script>
+
+
+
+
+   
