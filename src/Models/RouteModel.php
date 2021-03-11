1fae5700f73a025228195e68633fbefc27c972d8
José L. Gutiérrez <gutiecuador@gmail.com>
2021-03-10 21:12:43 -0500

    add fireworkds
    Hay un error ya que bloquea los elementos

--- src/Models/RouteModel.php
+++ src/Models/RouteModel.php
@@ -7,50 +7,18 @@ class routeModel {
             case 'bienvenidos':
                 $contenido = "views/modulos/bienvenidosView.php";
                 break;
-            
-            case 'register':
-                $contenido = "views/modulos/registerView.php";
-                break;
 
-            case 'comoparticipar':
-                $contenido = "views/modulos/comoparticiparView.php";
-                break;
-
-            case 'premios':
-                $contenido = "views/modulos/premiosView.php";
-                break;
-
-            case 'terminoscondiciones':
-                $contenido = "views/modulos/terminoscondicionesView.php";
+            case 'colombia':
+                $contenido = "views/modulos/colombiaView.php";
                 break;
                 
-            case 'dashboard':
-                $contenido = "views/modulos/dashboardView.php";
-                break;    
 
-            case 'puntos':
-                $contenido = "views/modulos/puntosView.php";
-                break;    
-                
-            case 'ranking':
-            $contenido = "views/modulos/rankingView.php";
-                break;    
-
-            case 'login':
-                $contenido = "views/modulos/loginView.php";
-                break;    
-         
-            case 'logout':
-                $contenido = "views/modulos/cerrarSesion.php";
-                break; 
-
-            // Social Logins
-            case 'loginfacebook':
-                $contenido = "views/modulos/facebookView.php";
+            case 'ecuador':
+                $contenido = "views/modulos/ecuadorView.php";
                 break;
             
-            case 'logingoogle':
-                $contenido = "views/modulos/googleView.php";
+            case 'peru':
+                $contenido = "views/modulos/peruView.php";
                 break;
             
             default:
---
+++ views/modulos/ecuadorView.php
@@ -0,0 +1,39 @@
+<div class="main-container" style="background: linear-gradient(135deg, #19518c 0%, #4582EC  100%);" >
+  
+</div>
+
+
+<!-- Required vendor scripts (Do not remove) -->
+<script type="text/javascript" src="assets/js/jquery.min.js"></script>
+<script type="text/javascript" src="assets/js/popper.min.js"></script>
+<script type="text/javascript" src="assets/js/bootstrap.js"></script>
+
+<!-- Optional Vendor Scripts (Remove the plugin script here and comment initializer script out of index.js if site does not use that feature) -->
+
+<!-- AOS (Animate On Scroll - animates elements into view while scrolling down) -->
+<script type="text/javascript" src="assets/js/aos.js"></script>
+<!-- Flatpickr (calendar/date/time picker UI) -->
+<script type="text/javascript" src="assets/js/flatpickr.min.js"></script>
+<!-- Flickity (handles touch enabled carousels and sliders) -->
+<script type="text/javascript" src="assets/js/flickity.pkgd.min.js"></script>
+<!-- jarallax (parallax effect and video backgrounds) -->
+<script type="text/javascript" src="assets/js/jarallax.min.js"></script>
+<script type="text/javascript" src="assets/js/jarallax-video.min.js"></script>
+<script type="text/javascript" src="assets/js/jarallax-element.min.js"></script>
+<!-- ScrollMonitor (manages events for elements scrolling in and out of view) -->
+<script type="text/javascript" src="assets/js/scrollMonitor.js"></script>
+<!-- jQuery smartWizard facilitates steppable wizard content -->
+<script type="text/javascript" src="assets/js/jquery.smartWizard.min.js"></script>
+<!-- Smooth scroll (animation to links in-page)-->
+<script type="text/javascript" src="assets/js/smooth-scroll.polyfills.min.js"></script>
+<!-- Prism (displays formatted code boxes) -->
+<script type="text/javascript" src="assets/js/prism.js"></script>
+<script type="text/javascript" src="assets/js/zoom.min.js"></script>
+
+<!-- Required theme scripts (Do not remove) -->
+<script type="text/javascript" src="assets/js/theme.js"></script>
+
+
+
+
+   
